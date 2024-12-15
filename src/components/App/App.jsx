ce0a7bf3633bfd968199a3f7dc92355665018020
PdaDmitry <146446934+PdaDmitry@users.noreply.github.com>
2024-12-15 12:36:17 +0200

    Create Header and Routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,68 +1,77 @@
-// import { useState } from 'react';
-// import './App.css';
-
-// function App() {
-//   const [count, setCount] = useState(0);
-
-//   return <>App!</>;
-// }
-
-// export default App;
-
-import { useState, useEffect } from 'react';
-import { getDatabase, ref, get, child } from 'firebase/database';
-import { app } from '../../../firebase'; // Путь к вашему firebase.js
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Header } from '../Header/Header';
+// import css from './App.module.css';
 
 function App() {
-  const [teachers, setTeachers] = useState([]); // Состояние для данных
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchTeachers = async () => {
-      try {
-        const db = getDatabase(app);
-        const dbRef = ref(db, '/'); // Путь к массиву в корне базы
-        const snapshot = await get(dbRef);
-
-        if (snapshot.exists()) {
-          setTeachers(snapshot.val()); // Сохраняем массив в состояние
-        } else {
-          console.log('No data available');
-        }
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchTeachers();
-  }, []);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
   return (
-    <div className="App">
-      <h1>Teachers</h1>
-      <ul>
-        {teachers.map((teacher, index) => (
-          <li key={index}>
-            <img src={teacher.avatar_url} alt={`${teacher.name} avatar`} width="100" />
-            <h2>
-              {teacher.name} {teacher.surname}
-            </h2>
-            <p>Languages: {teacher.languages.join(', ')}</p>
-            <p>Price per hour: ${teacher.price_per_hour}</p>
-            <p>Rating: {teacher.rating}</p>
-            <p>{teacher.lesson_info}</p>
-          </li>
-        ))}
-      </ul>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/teachers" element={<h1>Teachers</h1>} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
+// =======================================================================
+
+// import { useState, useEffect } from 'react';
+// import { getDatabase, ref, get, child } from 'firebase/database';
+// import { app } from '../../../firebase'; // Путь к вашему firebase.js
+// import './App.css';
+
+// function App() {
+//   const [teachers, setTeachers] = useState([]); // Состояние для данных
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     const fetchTeachers = async () => {
+//       try {
+//         const db = getDatabase(app);
+//         const dbRef = ref(db, '/'); // Путь к массиву в корне базы
+//         const snapshot = await get(dbRef);
+
+//         if (snapshot.exists()) {
+//           setTeachers(snapshot.val()); // Сохраняем массив в состояние
+//         } else {
+//           console.log('No data available');
+//         }
+//       } catch (error) {
+//         console.error('Error fetching data:', error);
+//       } finally {
+//         setLoading(false);
+//       }
+//     };
+
+//     fetchTeachers();
+//   }, []);
+
+//   if (loading) {
+//     return <div>Loading...</div>;
+//   }
+
+//   return (
+//     <div className="App">
+//       <h1>Teachers</h1>
+//       <ul>
+//         {teachers.map((teacher, index) => (
+//           <li key={index}>
+//             <img src={teacher.avatar_url} alt={`${teacher.name} avatar`} width="100" />
+//             <h2>
+//               {teacher.name} {teacher.surname}
+//             </h2>
+//             <p>Languages: {teacher.languages.join(', ')}</p>
+//             <p>Price per hour: ${teacher.price_per_hour}</p>
+//             <p>Rating: {teacher.rating}</p>
+//             <p>{teacher.lesson_info}</p>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,55 @@
+import { NavLink, useNavigate } from 'react-router-dom';
+import clsx from 'clsx';
+import css from './Header.module.css';
+
+const buildLinkClass = ({ isActive }) => {
+  return clsx(css.link, isActive && css.active);
+};
+
+export const Header = () => {
+  const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    navigate('/');
+  };
+
+  return (
+    <header className={css.contRout}>
+      <div className={css.logo} onClick={handleLogoClick}>
+        <svg className={css.logoSvg}>
+          <use href="/symbol-defs-before-registration.svg#icon-ukraine"></use>
+        </svg>
+        <p className={css.textLogo}>LearnLingo</p>
+      </div>
+      <nav>
+        <ul className={css.contUl}>
+          <li className={css.contLiHome}>
+            <NavLink to="/" className={buildLinkClass}>
+              Home
+            </NavLink>
+          </li>
+          <li className={css.contLiTeachers}>
+            <NavLink to="/teachers" className={buildLinkClass}>
+              Teachers
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <div className={css.contAuthentication}>
+        <button type="button" className={css.btnLogout}>
+          <svg className={css.logoutSvg}>
+            <use href="/symbol-defs-before-registration.svg#icon-log-in-01"></use>
+          </svg>
+        </button>
+
+        <button type="button" className={css.btnLogIn}>
+          Log in
+        </button>
+
+        <button type="button" className={css.btnRegistration}>
+          Registration
+        </button>
+      </div>
+    </header>
+  );
+};
