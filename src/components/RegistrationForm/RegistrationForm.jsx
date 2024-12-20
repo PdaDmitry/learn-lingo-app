e40b2c48cf1277bc514cb1e19360eaa8337c28ff
PdaDmitry <pda.gazcomp@gmail.com>
2024-12-20 13:05:45 +0200

    Create RegistrationForm.jsx and validationSchemas.js

---
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -0,0 +1,38 @@
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { userSchema } from '../../validationSchemas';
+
+export const RegistrationForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(userSchema),
+  });
+
+  const onSubmit = data => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <input {...register('name')} placeholder="Name" />
+        {errors.username && <p>{errors.username.message}</p>}
+      </div>
+
+      <div>
+        <input {...register('email')} placeholder="Email" />
+        {errors.email && <p>{errors.email.message}</p>}
+      </div>
+
+      <div>
+        <input type="password" {...register('password')} placeholder="Password" />
+        {errors.password && <p>{errors.password.message}</p>}
+      </div>
+
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+};
---
+++ src/validationSchemas.js
@@ -0,0 +1,26 @@
+import * as yup from 'yup';
+
+export const userSchema = yup.object().shape({
+  username: yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Required'),
+  email: yup.string().email('Must be a valid email!').required('Required'),
+  password: yup
+    .string()
+    .min(6, 'Password must be at least 6 characters')
+    .required('Password is required'),
+});
+
+export const loginSchema = yup.object().shape({
+  email: yup.string().email('Invalid email').required('Email is required'),
+  password: yup.string().required('Password is required'),
+});
+
+export const bookLesson = yup.object().shape({
+  fullName: yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Required'),
+  email: yup.string().email('Must be a valid email!').required('Required'),
+  number: yup
+    .string()
+    .min(3, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required')
+    .matches(/^[\d-]+$/, 'Number can only contain digits and dashes'),
+});
