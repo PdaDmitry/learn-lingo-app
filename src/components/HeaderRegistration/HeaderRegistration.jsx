a257f76b15b44f5cd05759f7e124326bde3183cb
PdaDmitry <146446934+PdaDmitry@users.noreply.github.com>
2024-12-25 19:30:01 +0200

    Create change theme

--- src/components/HeaderRegistration/HeaderRegistration.jsx
+++ src/components/HeaderRegistration/HeaderRegistration.jsx
@@ -2,23 +2,49 @@ import { NavLink, useNavigate } from 'react-router-dom';
 import clsx from 'clsx';
 
 import css from './HeaderRegistration.module.css';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { logoutUser } from '../../redux/auth/operationsAuth';
 import { clearFavorites } from '../../redux/teachers/teachersSlice';
 import { ChangeThemeForm } from '../ChangeThemeForm/ChangeThemeForm';
 import { useState } from 'react';
-// import { RegistrationForm } from '../RegistrationForm/RegistrationForm';
 import ModalWindow from '../ModalWindow/ModalWindow';
-// import { LogInForm } from '../LogInForm/LogInForm';
+import { selectUserTheme } from '../../redux/auth/selectorsAuth';
 
 const buildLinkClass = ({ isActive }) => {
   return clsx(css.link, isActive && css.active);
 };
 
+const colorDependence = {
+  '#F4C550': '#FBE9BA',
+  '#9FBAAE': '#CBDED3',
+  '#9FB7CE': '#BFD6EA',
+  '#E0A39A': '#F2C0BD',
+  '#F0AA8D': '#F4C8BA',
+};
+
+const iconDependence = {
+  '#F4C550': 1,
+  '#9FBAAE': 2,
+  '#9FB7CE': 3,
+  '#E0A39A': 4,
+  '#F0AA8D': 5,
+};
+
 export const HeaderRegistration = () => {
+  const [isHovered, setIsHovered] = useState(false);
   const navigate = useNavigate();
-  //   const [isModalOpen, setIsModalOpen] = useState(false);
+
   const [themeModalOpen, setThemeModalOpen] = useState(false);
+  const userTheme = useSelector(selectUserTheme);
+
+  const dynamicStyles = {
+    dinamicBackground: {
+      background: colorDependence[userTheme] || '#FBE9BA',
+    },
+    btnTheme: {
+      background: userTheme || '#F4C550',
+    },
+  };
 
   const dispatch = useDispatch();
 
@@ -31,8 +57,6 @@ export const HeaderRegistration = () => {
     dispatch(clearFavorites());
     navigate('/');
   };
-  //   const openModal = () => setIsModalOpen(true);
-  //   const closeModal = () => setIsModalOpen(false);
 
   const openThemeModal = () => setThemeModalOpen(true);
   const closeThemeModal = () => setThemeModalOpen(false);
@@ -67,12 +91,20 @@ export const HeaderRegistration = () => {
       <div className={css.contAuthentication}>
         <button type="button" className={css.btnLogOut} onClick={handleLogout}>
           <svg className={css.loginSvg}>
-            <use href="/symbol-defs-log-out.svg#icon-log-out-01"></use>
+            {/* <use href="/symbol-defs-log-out.svg#icon-log-out-03"></use> */}
+            <use href={`/symbol-defs-log-out.svg#icon-log-out-0${iconDependence[userTheme]}`} />
           </svg>
           Logout
         </button>
 
-        <button type="button" className={css.btnRegistration} onClick={openThemeModal}>
+        <button
+          type="button"
+          className={css.btnRegistration}
+          onClick={openThemeModal}
+          style={isHovered ? dynamicStyles.dinamicBackground : dynamicStyles.btnTheme}
+          onMouseEnter={() => setIsHovered(true)} // When the mouse hovers, change the state
+          onMouseLeave={() => setIsHovered(false)}
+        >
           Change theme
         </button>
       </div>
--- src/pages/HomeRegistration/HomeRegistration.jsx
+++ src/pages/HomeRegistration/HomeRegistration.jsx
@@ -7,18 +7,40 @@ import Avatar4 from '../../../public/block-4.jpg';
 import Avatar5 from '../../../public/block-5.jpg';
 import { useSelector } from 'react-redux';
 import { selectUserTheme } from '../../redux/auth/selectorsAuth';
+import { useState } from 'react';
+
+const Avatar = {
+  '#F4C550': Avatar1,
+  '#9FBAAE': Avatar2,
+  '#9FB7CE': Avatar3,
+  '#E0A39A': Avatar4,
+  '#F0AA8D': Avatar5,
+};
+
+const colorDependence = {
+  '#F4C550': '#FBE9BA',
+  '#9FBAAE': '#CBDED3',
+  '#9FB7CE': '#BFD6EA',
+  '#E0A39A': '#F2C0BD',
+  '#F0AA8D': '#F4C8BA',
+};
 
 export const HomeRegistration = () => {
+  const [isHovered, setIsHovered] = useState(false);
   const navigate = useNavigate();
   const userTheme = useSelector(selectUserTheme);
-  console.log('userTheme ', userTheme);
+  // console.log('userTheme ', userTheme);
 
-  const Avatar = {
-    '#F4C550': Avatar1,
-    '#9FBAAE': Avatar2,
-    '#9FB7CE': Avatar3,
-    '#E0A39A': Avatar4,
-    '#F0AA8D': Avatar5,
+  const dynamicStyles = {
+    dinamicBackground: {
+      background: colorDependence[userTheme] || '#FBE9BA',
+    },
+    btnStarted: {
+      background: userTheme || '#F4C550',
+    },
+    teacherProfile: {
+      border: `2px dashed ${userTheme}`,
+    },
   };
 
   const handleButtonClick = () => {
@@ -33,25 +55,31 @@ export const HomeRegistration = () => {
             Unlock your potential with the best <span className={css.textSpan}>language</span>{' '}
             tutors
           </h1>
-          <div className={css.backgroundSpan}></div>
+          <div className={css.backgroundSpan} style={dynamicStyles.dinamicBackground}></div>
           <p className={css.text}>
             Embark on an Exciting Language Journey with Expert Language Tutors: Elevate your
             language proficiency to new heights by connecting with highly qualified and experienced
             tutors.
           </p>
-          <button type="button" className={css.btnStarted} onClick={handleButtonClick}>
+          <button
+            type="button"
+            className={css.btnStarted}
+            onClick={handleButtonClick}
+            style={isHovered ? dynamicStyles.dinamicBackground : dynamicStyles.btnStarted}
+            onMouseEnter={() => setIsHovered(true)} // When the mouse hovers, change the state
+            onMouseLeave={() => setIsHovered(false)}
+          >
             Get started
           </button>
         </div>
         <img
           src={userTheme ? Avatar[userTheme] : Avatar['#F4C550']}
-          // src={Avatar[userTheme]}
           alt="Language learning"
           className={css.imgSvg}
         />
       </div>
 
-      <ul className={css.teacherProfile}>
+      <ul className={css.teacherProfile} style={dynamicStyles.teacherProfile}>
         <li className={css.contUlLi}>
           <p className={css.numberPlus}>32,000 +</p>
           <p className={css.teacherProfileText}>Experienced tutors</p>
